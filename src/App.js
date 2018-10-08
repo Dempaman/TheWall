3c415650c2b940862e9bdb90c8ba4abafed16266
Oskar Jonsson <oskarj97@hotmail.com>
2018-10-08 17:01:21 +0200

    stash

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import ProfileComp from './components/ProfileComp.js';
+import StatusWallComponent from './components/StatusWallComponent.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <ProfileComp />
+      <div>
+        <ProfileComp />
+
       </div>
     );
   }
