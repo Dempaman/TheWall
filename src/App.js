2833447d8baeb203a99657442ad9268d6145d9f9
Dempaman <32450932+Dempaman@users.noreply.github.com>
2018-10-10 12:57:32 +0200

    moved apiData state to App component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,42 @@ import Wall from './components/Wall/Wall.js';
 import Users from "./components/Users/Users.js";
 import Header from './components/Header.js';
 
+const APIDATA = 'http://localhost:4000/api/users'
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      apiData: [],
+      randomProfile: [],
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUsersFunction();
+  }
+
+  //Data från http://localhost:4000/api/users
+  fetchUsersFunction(){
+    fetch(APIDATA)
+    .then(response => response.json())
+    .then(data => this.setState(
+      {
+        apiData: data,
+        randomProfile: data[Math.floor(Math.random() * data.length)]
+      })
+    )
+  }
+
   render() {
     return (
       <div className="app">
         <Header />
         <div className="mainCompContainer">
-          <Profile />
+          <Profile
+            apiData={this.state.apiData}
+            randomProfile={this.state.randomProfile}
+          />
           <Wall />
           <Users />
         </div>
